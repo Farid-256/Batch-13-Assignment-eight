c44955c0007a0f3d2ffe005a739668eec6d985c7
Farid-256 <faridiqubal34fx@gmail.com>
2026-05-03 21:33:49 +0600

    loading done

--- src/app/(main)/loading.jsx
+++ src/app/(main)/loading.jsx
@@ -1,9 +1,9 @@
 
-
+// Loading-section
 const Loading = () => {
     return (
         <div className='flex justify-center items-center h-[80vh]'>
-            <h1 className='text-8xl font-bold text-blue-700'>Loading...</h1>
+            <h1 className='text-8xl font-bold text-blue-800'>Loading...</h1>
         </div>
     );
 };
